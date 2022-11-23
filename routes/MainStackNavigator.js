01526c4b57428cd39dd8f3042c9fe07929433be9
Gwonseon <202003789@hufs.ac.kr>
2022-11-23 11:28:32 +0900

    WeatherAPI

--- routes/MainStackNavigator.js
+++ routes/MainStackNavigator.js
@@ -19,4 +19,4 @@ function MainStackNavigator() {
     );
 }
 
-export default MainStackNavigator;
+export default MainStackNavigator; 
--- routes/MainTabNavigator.js
+++ routes/MainTabNavigator.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screen/Home';
 import Layout from '../screen/layout';
-
+import Weather from '../screen/Weather';
 
 const Nav = createBottomTabNavigator();
 
@@ -13,7 +13,7 @@ function MainTabNavigator() {
             <Nav.Navigator initialRouteName='Home'>
                 <Nav.Screen name = "Layout" component={Layout} options={{title: '처음 Layout'}}/>
                 <Nav.Screen name = "Home" component={Home} options={{title: '추가 Home'}}/>
-
+                <Nav.Screen name = "Weather" component={Weather} options={{title: '날씨조회화면'}}/>
             </Nav.Navigator>
         </NavigationContainer>
     );
